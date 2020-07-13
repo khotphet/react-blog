1149ed2ea5f2299a03ecf7205f8de00c92c68835
khotphet <profess538@gmail.com>
2020-07-13 14:06:22 +0700

    2 files modified, 1 added, 1 deleted

--- Hello.js
+++ Hello.js
@@ -2,17 +2,30 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import styled from "styled-components"
 
-class Hello extends Component{
-  render(){
-    return(
-      <div>
-      iofeh
-      </div>
-    )
-  }
-}
 
 const MainContainer = styled.header `
-  backgroup: url(../image/) no-repeat center/cover;
-`
-export default Hello
+
+  background-image: url("https://img.freepik.com/free-vector/abstract-technology-particle-background_52683-25766.jpg?size=626&ext=jpg") ;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  padding: 5px;
+  width: 1000px;
+  height: 400px;
+`;
+
+
+const Hello = ()=> {
+    return(
+   <MainContainer>
+      <h1>ป๋าเอ็ก</h1>
+    </MainContainer>
+  
+    )
+  }
+
+
+
+export default Hello;
